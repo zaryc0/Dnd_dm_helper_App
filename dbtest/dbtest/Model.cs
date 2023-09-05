f5a7634b811083aa1ce8c79423a77a0f2d75ce64
Zaryc0 <cotgray1997@gmail.com>
2023-09-05 21:05:37 +0100

    dummy demo apps and 1st quick draft of requirements

---
+++ dbtest/dbtest/Model.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbtest
+{
+    public class dndContext : DbContext
+    {
+        public DbSet<Monster> Monsters { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+        public string DbPath { get; }
+
+        public dndContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "dndhelperapp.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Monster
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }
+    } 
+    public class Player
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; }
+    }
+}
---
+++ dbtest/dbtest/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
